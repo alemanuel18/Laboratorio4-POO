9bd1543469cce564333e3536899fa397be6fb96e
alemanuel18 <jer24678@uvg.edu.gt>
2024-11-08 12:11:27 -0600

    Se creo la clase interfaz ClaseA

---
+++ ClaseA.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public interface ClaseA{
+    
+    String Cambiar(boolean sonido);
+    String PlanificarViajes(Date inici, Date fin, String lugar_inicio, String lugar_final);
+}
